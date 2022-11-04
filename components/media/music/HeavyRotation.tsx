1e474a4051bb7454bf593df127efb951eb986094
Cory Dransfeldt <hi@coryd.dev>
2022-11-04 12:36:05 -0700

    move limit to api request

--- components/media/music/HeavyRotation.tsx
+++ components/media/music/HeavyRotation.tsx
@@ -4,8 +4,7 @@ import { Album } from '@/components/media/music/visual/Album'
 
 export const HeavyRotation = () => {
     const { response, error } = useJson('/api/music/heavy-rotation')
-    const ALBUM_LIMIT = 6
-    const albums = response?.data?.slice(0, ALBUM_LIMIT)
+    const albums = response?.data
     const HeavyRotation = (
         <>
             <h3 className="pt-3 pb-4 text-xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-2xl sm:leading-10 md:text-4xl md:leading-14">
--- pages/api/music/heavy-rotation.ts
+++ pages/api/music/heavy-rotation.ts
@@ -1,11 +1,15 @@
 export default async function handler(req, res) {
     const bearer = `Bearer ${process.env.API_APPLE_MUSIC_TOKEN}`
     const token = process.env.API_APPLE_MUSIC_USER_TOKEN
-    const data = await fetch(`https://api.music.apple.com/v1/me/history/heavy-rotation`, {
-        headers: {
-            Authorization: bearer,
-            'Music-User-Token': token,
-        },
-    }).then((response) => response.json())
+    const ALBUM_LIMIT = 6
+    const data = await fetch(
+        `https://api.music.apple.com/v1/me/history/heavy-rotation?limit=${ALBUM_LIMIT}`,
+        {
+            headers: {
+                Authorization: bearer,
+                'Music-User-Token': token,
+            },
+        }
+    ).then((response) => response.json())
     res.json(data)
 }
