eadbe89be835c77dff2cc44a092a2675b28cc88b
Cory Dransfeldt <hi@coryd.dev>
2023-02-16 17:05:21 -0800

    fix path

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -4,7 +4,7 @@ import { ComponentMap, getMDXComponent } from 'mdx-bundler/client'
 import Image from 'next/image'
 import TOCInline from './TOCInline'
 import Pre from './Pre'
-import Paste from '@/components/omg/Paste'
+import Paste from './omg/Paste'
 
 const Wrapper: React.ComponentType<{ layout: string }> = ({ layout, ...rest }) => {
     const Layout = require(`../layouts/${layout}`).default
