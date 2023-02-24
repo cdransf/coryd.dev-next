076f9d3ac6291e10b7c46b9769a2acb8d9d808e0
Cory Dransfeldt <hi@coryd.dev>
2023-02-24 09:55:31 -0800

    remove dep

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ import * as Fathom from 'fathom-client'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 import { useEffect } from 'react'
+import ErrorBoundary from '@/components/ErrorBoundary'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
@@ -34,14 +35,16 @@ export default function App({ Component, pageProps }: AppProps) {
     }, [router.events])
 
     return (
-        <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-            <Head>
-                <meta content="width=device-width, initial-scale=1" name="viewport" />
-            </Head>
-            {isDevelopment && isSocket && <ClientReload />}
-            <LayoutWrapper>
-                <Component {...pageProps} />
-            </LayoutWrapper>
-        </ThemeProvider>
+        <ErrorBoundary>
+            <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+                <Head>
+                    <meta content="width=device-width, initial-scale=1" name="viewport" />
+                </Head>
+                {isDevelopment && isSocket && <ClientReload />}
+                <LayoutWrapper>
+                    <Component {...pageProps} />
+                </LayoutWrapper>
+            </ThemeProvider>
+        </ErrorBoundary>
     )
 }
