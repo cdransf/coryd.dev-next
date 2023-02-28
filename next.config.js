52d597811b57cbb1c78b667e4780f255dd251e69
Cory Dransfeldt <hi@coryd.dev>
2023-02-28 13:51:51 -0800

    csp fix

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' breezy-restored.coryd.dev;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' breezy-restored.coryd.dev;
     style-src 'self' 'unsafe-inline';
     img-src * blob: data:;
     media-src 'none';
